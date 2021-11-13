7594ac097404732f27917922f736306637f6e0e1
chylex <contact@chylex.com>
2021-11-13 12:59:21 +0100

    Update to 1.18 pre-release

--- src/main/java/chylex/viewdistance/mixin/ChunkMapAccessor.java
+++ src/main/java/chylex/viewdistance/mixin/ChunkMapAccessor.java
@@ -1,14 +1,15 @@
 package chylex.viewdistance.mixin;
 
-import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkWithLightPacket;
 import net.minecraft.server.level.ChunkMap;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.ChunkPos;
+import org.apache.commons.lang3.mutable.MutableObject;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ChunkMap.class)
 public interface ChunkMapAccessor {
 	@Invoker
-	void callUpdateChunkTracking(final ServerPlayer serverPlayer, final ChunkPos pos, final Packet<?>[] packetArrayForReuse, final boolean stopTracking, final boolean startTracking);
+	void callUpdateChunkTracking(final ServerPlayer serverPlayer, final ChunkPos pos, final MutableObject<ClientboundLevelChunkWithLightPacket> chunkPacket, final boolean stopTracking, final boolean startTracking);
 }
